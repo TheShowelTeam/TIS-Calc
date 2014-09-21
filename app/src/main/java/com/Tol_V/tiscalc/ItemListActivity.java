3418aee0d95cfec546722a9325373925d8c868fe
Tol-v <tol-v@ya.ru>
2014-09-22 00:22:27 +0400

    Added Saving Text to file

--- app/src/main/java/com/Tol_V/tiscalc/ItemListActivity.java
+++ app/src/main/java/com/Tol_V/tiscalc/ItemListActivity.java
@@ -4,13 +4,20 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
@@ -54,8 +61,53 @@ public class ItemListActivity extends Activity {
 
     private void writeList(){
         //do stuff
+        ChooseFileNameDialog();
     }
 
+
+    private void writeToFile(String filePath)
+    {
+        if (filePath.equals("")) {
+            Log.d("writeList()", "empty input");
+            return;
+        }
+        if (!Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            Log.d("writeList()", "SD-карта не доступна: " + Environment.getExternalStorageState());
+            return;
+        }
+        // получаем путь к SD
+        File sdPath = Environment.getExternalStorageDirectory();
+        // добавляем свой каталог к пути
+        sdPath = new File(sdPath.getAbsolutePath() + "/" + "TISCalc(results)");
+        // создаем каталог
+        sdPath.mkdirs();
+        // формируем объект File, который содержит путь к файлу
+        File sdFile = new File(sdPath, filePath+".txt");
+        try {
+            // открываем поток для записи
+            BufferedWriter bw = new BufferedWriter(new FileWriter(sdFile));
+            // пишем данные
+            SelectedItems selectedItems = SelectedItems.getInstance();
+            String tab = "\t";
+            bw.write("Название" + tab + "Коммент." + tab + "Цена" + tab + "Кол-во" + tab + "Стоимость\n");
+            bw.write("--------------------------------------\n");
+            for (int i = 1; i < selectedItems.getItems().size(); i++)
+            {
+                String name = selectedItems.getFinalItem(i).getName();
+                String count = Integer.toString(selectedItems.getFinalItem(i).getCount());
+                String comment = selectedItems.getFinalItem(i).getComment();
+                String cost = Float.toString((float)selectedItems.getFinalItem(i).getPrice()/100);
+                String price = Float.toString((float)selectedItems.getFinalItem(i).getFullPrice()/100);
+                bw.write(name + tab + comment + tab + cost + tab + count + tab + price + "\n");
+            }
+            // закрываем поток
+            bw.close();
+            Log.d("writeList()", "Файл записан на SD: " + sdFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     private  void DeleteDialog(final FinalItem tmp)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(ItemListActivity.this);
@@ -85,6 +137,33 @@ public class ItemListActivity extends Activity {
         alert.show();
     }
 
+    private void ChooseFileNameDialog()
+    {
+        String res = "";
+        final EditText input = new EditText(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ItemListActivity.this);
+        builder.setTitle("Удаление")
+                .setMessage("Введите имя файла (фирмы)")
+                .setCancelable(false)
+                .setNegativeButton("Отмена",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        })
+                .setPositiveButton("Сохранить",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                writeToFile(input.getText().toString());
+                                dialog.cancel();
+                            }
+                        }
+                );
+        AlertDialog alert = builder.create();
+        alert.setView(input);
+        alert.show();
+    }
 
     private void drawList() {
         LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout2);
