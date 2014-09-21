68e4421985424283589895ecbef380835fa7aa04
Andry Eroshkin <drontosaur@ya.ru>
2014-09-21 20:53:54 +0400

    Added list of views for items in SelectedItems.

--- app/src/main/java/com/Tol_V/tiscalc/ItemListActivity.java
+++ app/src/main/java/com/Tol_V/tiscalc/ItemListActivity.java
@@ -2,8 +2,12 @@ package com.Tol_V.tiscalc;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class ItemListActivity extends Activity {
@@ -12,10 +16,35 @@ public class ItemListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        drawList();
     }
 
+    private void drawList() {
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout2);
+        linLayout.removeAllViews();
 
-    @Override
+        LayoutInflater ltInflater = getLayoutInflater();
+
+        ArrayList<FinalItem> items = SelectedItems.getInstance().getItems();
+
+        for(FinalItem cur: items){
+            View item = ltInflater.inflate(R.layout.list_item_extended, linLayout, false);
+
+            TextView tvName = (TextView) item.findViewById(R.id.tvNameExt);
+            TextView tvCount = (TextView) item.findViewById(R.id.tvCountExt);
+            TextView tvPrice = (TextView) item.findViewById(R.id.tvPriceExt);
+            TextView tvFullPrice = (TextView) item.findViewById(R.id.tvFullPriceExt);
+
+            tvName.setText("Name: " + cur.getName());
+            tvCount.setText("Count: " + Integer.toString(cur.getCount()));
+            tvPrice.setText("Cost: " + Integer.toString(cur.getPrice()));
+            tvFullPrice.setText("Price: " + Integer.toString(cur.getFullPrice()));
+
+            linLayout.addView(item);
+        }
+    }
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.item_list, menu);
@@ -32,5 +61,5 @@ public class ItemListActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
